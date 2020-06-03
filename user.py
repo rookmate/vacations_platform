d044a9b8c07c7783a76dd24f4f055387240b718f
rookmate <14072042+rookmate@users.noreply.github.com>
2020-06-03 12:07:31 +0100

    Add User class skeleton

---
+++ user.py
@@ -0,0 +1,28 @@
+from days_off import Vacations
+
+# This class will be the representation of a user. It will sanitize the data
+# to add and input users in the database.
+class User(Vacations):
+    status = 'active'
+
+    def __init__(self, firstn, lastn, birthd, entry, firstyear=True, totaldays=22, override=True):
+        super(User, self).__init__(bool(firstyear), int(totaldays), bool(override))
+        self.first_name = str(firstn)
+        self.last_name = str(lastn)
+        self.birth_date = str(birthd)
+        self.entry_date = str(entry)
+
+    def __repr__(self):
+        return {'first name': self.first_name,
+                'last name': self.last_name,
+                'birth_date': self.birth_date,
+                'date of entry': self.entry_date,
+                'status': self.status,
+                'Vacation details': super(User, self).__repr__()
+                }
+
+
+
+if __name__ == "__main__":
+    u1 = User('Esdrubal', 'Petra', '1990-05-08', '2020-02-01', "Tue", 22, True)
+    print(u1.__repr__())
